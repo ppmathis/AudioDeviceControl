ce2585f55a0adfeaa3ede5ed0d7960e29a111ecd
Pascal Mathis <dev@snapserv.net>
2015-04-27 20:30:29 +0200

    Endpoint index will now be shown in output

--- AudioDeviceControl.cpp
+++ AudioDeviceControl.cpp
@@ -12,10 +12,12 @@ int wmain(int argc, wchar_t* argv[])
 
 	for (std::vector<AudioEndpoint*>::iterator iEndpoint = endpoints.begin(); iEndpoint != endpoints.end(); ++iEndpoint) {
 		AudioEndpoint *endpoint = *iEndpoint;
-		std::wcout << L"UUID: " << endpoint->GetUuid() << std::endl;
-		std::wcout << L"Friendly Name: " << endpoint->GetFriendlyName() << std::endl;
-		std::wcout << L"Samples per Second: " << endpoint->GetSamplesPerSec() << std::endl;
-		std::wcout << L"Bits per Sample: " << endpoint->GetBitsPerSample() << std::endl;
+		std::wcout << L"Endpoint #" << (iEndpoint - endpoints.begin()) << std::endl;
+		std::wcout << L"> UUID: " << endpoint->GetUuid() << std::endl;
+		std::wcout << L"> Friendly Name: " << endpoint->GetFriendlyName() << std::endl;
+		std::wcout << L"> Samples per Second: " << endpoint->GetSamplesPerSec() << std::endl;
+		std::wcout << L"> Bits per Sample: " << endpoint->GetBitsPerSample() << std::endl;
+		std::wcout << std::endl;
 	}
 
 	std::cin.ignore();
